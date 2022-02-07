58efe2ce716d5c63c2775daf55ce46b61e3f0da6
tzyn <tzyn.wang@gmail.com>
2022-02-07 21:58:57 +0800

    [feat] Update App

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,9 +2,10 @@ import React, { memo } from 'react'
 
 import Layout from '@Components/Layout'
 import ManualError from '@Components/Common/Error'
+import AsyncError from '@Components/Common/AsyncError'
 
 function App(): React.ReactElement {
-  return <Layout header={'header'} main={<ManualError />} column={'column'} />
+  return <Layout header={<AsyncError />} main={'main'} column={<ManualError />} />
 }
 
 export default memo(App)
